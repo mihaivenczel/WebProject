2c9869a897c45f157e99dca86daa604757b47340
mihaivenczel <mihaivenczel@yahoo.com>
2021-01-14 22:55:26 +0200

    WIP | Added the first concept of auth

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
  */
 Route::group(['middleware' => ['web']], function () {
+    //Auth routes
+    Route::get('auth/login', 'Auth\AuthController@showLoginForm');
+    Route::post('auth/login', 'Auth\AuthController@postLogin');
+    Route::get('auth/logout', 'Auth\AuthController@postLogout');
+
+    //Register routes
+    Route::get('auth/logout','Auth\RegisterController@showRegistrationForm');
+    Route::post('auth/logout','Auth\AuthController@postRegister');
+
+
+    //Everything else
     Route::get('webproject/{slug}', ['as' => 'blog.single', 'uses' => 'App\Http\Controllers\BlogController@getSingle'])->where('slug', '[w\d\-\_]+');
-    Route::get('blog',['uses'=>'App\Http\Controllers\BlogController@getIndex', 'as'=>'blog.index']);
+    Route::get('blog', ['uses' => 'App\Http\Controllers\BlogController@getIndex', 'as' => 'blog.index']);
     Route::get('/', 'App\Http\Controllers\PageController@getIndex');
     Route::get('about', 'App\Http\Controllers\PageController@getAbout');
     Route::resource('/posts', 'App\Http\Controllers\PostController');
